ddc1c41daae1c3f87ea60d2b7f3f195ae51edfac
athreshanju <athreshanju@msitprogram.net>
2020-04-16 17:06:03 +0530

    done registration page

--- application.py
+++ application.py
@@ -1,17 +1,19 @@
 import os
+import sys
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request,redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
+# Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-
+# Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -23,4 +25,17 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return redirect("/register")
+
+@app.route("/register")
+def register():
+	return render_template("reg.html")
+
+
+@app.route("/user",methods=["POST"])
+def user():
+	username = request.form.get("username")
+	password = request.form.get("pass")
+	print(username,file=sys.stderr)
+	return render_template("pager.html", name=username)
+
