d1009ac3731b0ea7898ed792a0dcde7392279434
athreshanju <athreshanju@msitprogram.net>
2020-04-20 22:19:32 +0530

    implemented authentication for users

--- application.py
+++ application.py
@@ -1,26 +1,100 @@
 import os
+import sys
+import time
 
-from flask import Flask, session
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-app = Flask(__name__)
+from users import *
+from flask import Flask, session, render_template, request,redirect,url_for
+from sqlalchemy import create_engine,desc
 
 
+
+app = Flask(__name__, static_url_path='/static')
+app.secret_key = 'khammam'
+
+
+# Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+# Configure session to use filesystem
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+    
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+
 
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    if 'username' in session:
+        return redirect(url_for('home'))
+    return redirect(url_for('register'))
+
+
+@app.route("/register")
+def register():
+    return render_template("reg.html")
+
+
+@app.route("/User",methods=["POST","GET"])
+def User():
+    if request.method == "POST":
+        Username = request.form.get("username")
+        Password = request.form.get("pass")
+        Email = request.form.get("email")
+        print(Username,file=sys.stderr)
+        obj = user.query.filter_by(email = Email).first()
+        if obj is None:
+            usr = user(username = Username,email = Email,password = Password,time = time.ctime(time.time()))
+            db.session.add(usr)
+            db.session.commit()
+            return render_template("pager.html",name = Username,email = Email)
+        else:
+            # print()
+            return render_template("reg.html", message = "email already exists!")
+    if request.method == "GET":
+        return "<h1> please register yourself </h1>"
+
+@app.route("/admin")
+def admin():
+    adm = user.query.order_by(desc(user.time)).all()
+    return render_template("admin.html",adm = adm)
+
+@app.route("/auth",methods=["POST","GET"])
+def auth():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("pass")
+
+        userdata = user.query.filter_by(username=username).first()
+
+        if userdata is not None:
+            if userdata.username == username and userdata.password == password:
+                session[username] = username
+                return redirect(url_for('userhome',user=username))
+            else:
+                return render_template("reg.html", message = "username or password is incorrect")
+        else:
+            return redirect(url_for('index'))
+
+    else:
+        return "<h1> please login / register</h1>"
+
+@app.route("/home/<user>")
+def userhome(user):
+    if user in session:
+        return render_template("login.html",username = user,message="Sucessfully logged in : welcome!!")
+
+    return redirect(url_for('index'))
+
+@app.route("/logout/<username>")
+def logout(username):
+    session.pop(username, None)
+    return redirect(url_for('index'))
+
+
