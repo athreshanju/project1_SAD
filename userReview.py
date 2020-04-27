7b3f23f77ac8db847653aa51764078783866a54a
akhilalla94 <akhilalla1994@msitprogram.net>
2020-04-28 00:16:12 +0530

    Implemented book feature

--- userReview.py
+++ userReview.py
@@ -4,8 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-
 class review(db.Model):
     __tablename__ = "reviews"
     isbn = db.Column(db.String, nullable=False, primary_key=True)
