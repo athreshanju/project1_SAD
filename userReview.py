9f58b724caed16c540b6cf8a7d8901f4feb1925c
akhilalla94 <akhilalla1994@msitprogram.net>
2020-04-28 00:12:21 +0530

    Implemented review feature

--- userReview.py
+++ userReview.py
@@ -5,17 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# app = Flask(__name__, static_url_path='/static')
-# # Check for environment variable
-# if not os.getenv("DATABASE_URL"):
-#     raise RuntimeError("DATABASE_URL is not set")
-
-# # Configure session to use filesystem
-# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# db.init_app(app)
-# with app.app_context():
-#     db.create_all()
 
 class review(db.Model):
     __tablename__ = "reviews"
